d47cad17d75c8d8cea14ea0dc699c3bb3ba63dd7
Enderson Vizcaino <enjaviga@gmail.com>
2020-01-11 23:30:34 -0500

    Add TasksModule :ok_hand:

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://mongo_db:27017/my_app_nest_docker', { useNewUrlParser: true, useUnifiedTopology: true }),
+    TasksModule,
   ],
   controllers: [AppController],
   providers: [AppService],
