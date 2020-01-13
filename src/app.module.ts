247d4e1c8ca59d0a5aebb365f0f50a95a90966f4
Enderson Vizcaino <enjaviga@gmail.com>
2020-01-13 18:28:19 -0500

    configuracion para graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TasksModule } from './tasks/tasks.module';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
   imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+    }),
     MongooseModule.forRoot('mongodb://mongo_db:27017/my_app_nest_docker', { useNewUrlParser: true, useUnifiedTopology: true }),
     TasksModule,
   ],
