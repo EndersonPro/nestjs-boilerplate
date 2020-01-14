d283080968a6cfa433178d95b38577f98f71bb13
Enderson Vizcaino <enjaviga@gmail.com>
2020-01-13 20:20:00 -0500

    añadiendo metodo de obtener una tarea

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,22 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-
+import { Model } from 'mongoose';
+import { Task } from './task.interface';
+import { TaskType } from './dto/task.dto';
+import { TaskInput } from './input/tasks.input';
 @Injectable()
 export class TasksService {
-    constructor( @InjectModel('Tasks') private taskModel) {}
+    constructor( @InjectModel('Tasks') private taskModel: Model<Task>) {}
 
-    getTasks = async () => await this.taskModel.find();
+    getTasks = async (): Promise<TaskType[]> => await this.taskModel.find();
 
-    createTask = async (body) => {
+    getTask = async (id: string): Promise<TaskType> => await this.taskModel.findById(id);
+
+    createTask = async (body: TaskType): Promise<TaskType> => {
         const newTask = new this.taskModel(body);
         return await newTask.save();
     }
 
-    editTask = async (id, body) => {
+    editTask = async (id: string, body: TaskInput): Promise<TaskType> => {
         return await this.taskModel.findOneAndUpdate({ _id: id }, body, { new: true });
     }
 
-    deleteTask = async (id) => {
+    deleteTask = async (id: string): Promise<TaskType> => {
         return await this.taskModel.findOneAndRemove({ _id: id });
     }
 }
